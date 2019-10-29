27c457f3bf6e0b7f491b2ed1bd0484176c6622a4
GH-Song <anfflrnrgh@gmail.com>
2019-10-30 02:53:49 +0900

    numpy배열 이용중

--- Face_rec_mouth_det/facerec_2019.10.29.py
+++ Face_rec_mouth_det/facerec_2019.10.29.py
@@ -22,19 +22,32 @@ class speak_utils:
         self.color_b = 255
         self.specific_value1 = range(0,7)
         self.specific_value2 = range(0,7)
+        self.specific_values = np.zeros((7,20), dtype = np.float64)
         self.time1 = 0
         self.time2 = 0
         self.Mouth_movement = 0
+        self.sx = 0
+        self.sy = 0
+        self.ex = 0
+        self.ey = 0
+        self.Inmarks = []
+        self.Outmarks = []
+        self.Midmark = []
+
     def landmark(self, gray, sx=0, sy=0, ex=0, ey=0):
         '''검출된 얼굴 좌표들을 저장'''
-        rect = dlib.rectangle(left=int(sx), top=int(sy), right=int(ex), bottom=int(ey))
+        # grab the indexes of the facial landmarks for the left and
+        self.sx = int(sx);    self.sy = int(sy)
+        self.ex = int(ex);    self.ey = int(ey)
+        outmark_start, outmark_end = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+        inmark_start, inmark_end = face_utils.FACIAL_LANDMARKS_IDXS["inner_mouth"]
+
+        rect = dlib.rectangle(left=self.sx, top=self.sy, right=self.ex, bottom=self.ey)
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
-        self.lefttop = (sx, sy)
-        self.rightbottom = (ex, ey)
-        self.Inmarks = shape[innermouth_start:innermouth_end]
-        self.Outmarks = shape[innermouth_start:innermouth_end]
-        self.midmark = shape[28:29]
+        self.Inmarks = shape[inmark_start:inmark_end]
+        self.Outmarks = shape[outmark_start: outmark_end]
+        self.Midmark = shape[28:29]
         return
 
     def speakerfind():
@@ -62,16 +75,13 @@ class speak_utils:
 
     def face_area(self):
         # 얼굴 면적 계산하는 함수
-        startX = self.lefttop[0]
-        startY = self.lefttop[1]
-        endX = self.rightbottom[0]
-        endY = self.rightbottom[1]
 
-        X = dist.euclidean(startX,endX)
-        Y = dist.euclidean(startY,endY)
+        X = dist.euclidean(self.sx, self.ex)
+        Y = dist.euclidean(self.sy, self.ey)
 
         self.face_area = X*Y
         return self.face_area
+
     # 바깥 입술 면적 대비 벌린 면적 비율?
     def outtermouth_area(self):
         mouth = self.Outmarks
@@ -143,17 +153,27 @@ class speak_utils:
     def distance_factor(self, Innermouth, Outtermouth):
         return
 
-    def update_specific_value(self, key):
-        if key == 1:
+    def update_specific_value(self, option):
+        if option == 1:
             self.specific_value1 = [self.imar, self.OB, self.OC, self.OD, self.OF]
         else:
             self.specific_value2 = [self.imar, self.OB, self.OC, self.OD, self.OF]
 
+    def print_specific_values(self):
+        print(self.specific_values[:5,:5])
+
+    def update_specific_values(self, row=0):
+        self.specific_values[row,:5] = [self.imar, self.OB, self.OC, self.OD, self.OF]
+
+    def specific_values_cal(self):
+        np.sum(self.specific_values[1,:5])
+        array1 = self.specific_values[1,:5]
     def differential(self, timeref, option):
         '''Mouth_movement 값을 계산, 계산시 True반환'''
         if option == 1:
             # 계산방식 1
             del_time = self.time2 - self.time1
+            print(del_time)
             if del_time > timeref:
                 # 각 specific value에 대해, 가중치를 고려해서 입 움직임의 정도를 수치화
                 self.Mouth_movement = (abs(self.specific_value2[0] - self.specific_value1[0])*30 +
@@ -162,19 +182,22 @@ class speak_utils:
                                         abs(self.specific_value2[3] - self.specific_value1[3]) +
                                         abs(self.specific_value2[4] - self.specific_value1[4])*2
                                         )/ (del_time*110)
-                self.time1[name] = self.time2[name]
+                self.time1 = self.time2
                 self.specific_value1 = self.specific_value2
                 return True
             else:
                 return False
         elif option == 2:
             # 계산방식 2
-        return False
-    def calculate_itself(self):
-        self.face_area()
+            return False
+
+    def calculate_self(self, row = 0):
+        # self.face_area()
         self.innermouth_aspect_ratio() # self.imar
         self.outtermouth_distfactor() # self.A,B,C..
         self.update_specific_value(2) # 위의 계산을 time2의 specific value에 반영
+        self.update_specific_values(row)
+        self.print_specific_values()
         return
 
     def refresh(self, option):
@@ -193,10 +216,7 @@ class speak_utils:
 names = ["Song_GH", "Kim_JW", "Choi_EH", "Unknown"]
 
 # 각 이름들로 찾을 수 있는 speak_utils의 인스턴스의 딕셔너리 생성
-man = {}
-for key in names:
-    # 딕셔너리의 각 키에 대해 speak_utill의 인스턴스 부여
-    man[key] = speak_utils(key)
+man = {name: speak_utils(name) for name in names}
 
 # {"Song_GH":inst1 , "Kim_JW":inst2, "Choi_EH":inst3, "Unknown":inst4}
 # Man["Song_GH"] == inst1
@@ -214,59 +234,58 @@ FRAMES = 1
 def getTime(s):
     ss = s / 1
     return ss
-def initial_setup():
-    # 파일 및 폴더 경로 지정
-    PATH_predictor = "shape_predictor_68_face_landmarks.dat"
-    PATH_facedetection='face_detection_model'
-    PATH_recognizer = 'output/recognizer.pickle'
-    PATH_le = 'output/le.pickle'
 
-    # grab the indexes of the facial landmarks for the left and
-    outermouth_start, outermouth_end = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
-    innermouth_start, innermouth_end = face_utils.FACIAL_LANDMARKS_IDXS["inner_mouth"]
+# 파일 및 폴더 경로 지정
+PATH_predictor = "shape_predictor_68_face_landmarks.dat"
+PATH_facedetection='face_detection_model'
+PATH_recognizer = 'output/recognizer.pickle'
+PATH_le = 'output/le.pickle'
 
-    # initialize dlib's face detector (HOG-based) and then create
-    print("[INFO] loading facial landmark predictor...")
-    predictor = dlib.shape_predictor(PATH_predictor)
+# grab the indexes of the facial landmarks for the left and
+outmark_start, outmark_end = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+inmark_start, inmark_end = face_utils.FACIAL_LANDMARKS_IDXS["inner_mouth"]
 
-    # initialize the video stream and allow the cammera sensor to warmup
-    print("[INFO] camera sensor warming up...")
-    vs = VideoStream().start()
-    time.sleep(1.0)
+# initialize dlib's face detector (HOG-based) and then create
+print("[INFO] loading facial landmark predictor...")
+predictor = dlib.shape_predictor(PATH_predictor)
 
-    # load our serialized face detector from disk
-    print("[INFO] loading face detector...")
-    protoPath = os.path.sep.join([PATH_facedetection, "deploy.prototxt"])
-    modelPath = os.path.sep.join([PATH_facedetection, "res10_300x300_ssd_iter_140000.caffemodel"])
-    detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+# initialize the video stream and allow the cammera sensor to warmup
+print("[INFO] camera sensor warming up...")
+vs = VideoStream().start()
+time.sleep(1.0)
 
-    # load our serialized face embedding model from disk
-    print("[INFO] loading face recognizer...")
-    embedder = cv2.dnn.readNetFromTorch('openface_nn4.small2.v1.t7')
+# load our serialized face detector from disk
+print("[INFO] loading face detector...")
+protoPath = os.path.sep.join([PATH_facedetection, "deploy.prototxt"])
+modelPath = os.path.sep.join([PATH_facedetection, "res10_300x300_ssd_iter_140000.caffemodel"])
+detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
 
-    # load the actual face recognition model along with the label encoder
-    recognizer = pickle.loads(open(PATH_recognizer, "rb").read())
-    le = pickle.loads(open(PATH_le, "rb").read())
+# load our serialized face embedding model from disk
+print("[INFO] loading face recognizer...")
+embedder = cv2.dnn.readNetFromTorch('openface_nn4.small2.v1.t7')
 
-    # initialize the video stream, then allow the camera sensor to warm up
-    print("[INFO] starting video stream...")
-    vs = VideoStream(src=0).start()
-    time.sleep(1.0)
+# load the actual face recognition model along with the label encoder
+recognizer = pickle.loads(open(PATH_recognizer, "rb").read())
+le = pickle.loads(open(PATH_le, "rb").read())
 
-    # start the FPS throughput estimator
-    fps = FPS().start()
+# initialize the video stream, then allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(1.0)
+
+# start the FPS throughput estimator
+fps = FPS().start()
 #endregion
 # </editor-fold>
-
-initial_setup()
 
 # 시간 동기화
 First_time = getTime(time.time())
 for key in names:
     man[key].time1 = getTime(time.time())
-
 loopnumber = 0
 man_timeset = True
+test_loop = 0
+printnames = []
 
 # loop over frames from the video file stream
 while True:
@@ -284,6 +303,9 @@ while True:
         for key in names:
             man[key].time1 = getTime(time.time())
         man_timeset = False
+        test_loop = 0
+    else:
+        test_loop += 1
 
     # construct a blob from the image
     imageBlob = cv2.dnn.blobFromImage(
@@ -331,23 +353,24 @@ while True:
 
             # 이름이 부여되는 시점
             name = le.classes_[j]
-
+            printnames.append(name)
             # facial landmark 추출해서 인스턴스 멤버에 저장
             # self.inmarks, self.outmarks, self.midmark 값이 부여됨
             man[name].landmark(gray, startX, startY, endX, endY)
 
             # 시간 미분 구하기 - 클래스 함수로
-
+            
             # 미분 전에 미리 해줘야 할 것들
             man[name].time2 = getTime(time.time()) # 시간 바깥 while문에서, 해당 for문으로 들어오면서,
-            man[name].calculate_itself()
+            man[name].calculate_self(test_loop)
+            print(test_loop)
 
             # 미분 함수 호출
             # time2 - time1에 대해, specific_value의 변화 계산
             # self.Mouth_movement에 반영
             # 이 부분에 if loopnumber조건 추가 가능
             # 계산시 True가 되어 while문에서 time1 초기화
-            man_timeset = man[name].differential(0.01, 1) # time2 - time1이 0.01보다 큰지 고려해서 실행됨
+            man_timeset = man[name].differential(0.05, 1) # time2 - time1이 0.01보다 큰지 고려해서 실행됨
 
             # 역치값과 비교
             if man[name].Mouth_movement > TH_of_Movement:
@@ -357,7 +380,7 @@ while True:
     # 이름 기준으로 반복
     Last_time = getTime(time.time())
     # 2초동안 들린 소리가 누구 것에 가까운지 판단
-    if (Last_time - First_time) > 1.5:
+    if (Last_time - First_time) > 1:
         # 이전 검사 완료 이후 x초가 지났으면
         TOTAL_SUB_list = []
         # 모든 사람 인스턴스가 가진 self.TOTAL_SUB값을 리스트로 추출
@@ -387,26 +410,28 @@ while True:
     ######################출력값 지정##############################
     #print(TOTAL_SUB.items())
     # 만들어줘야 할 변수: starx-endy,
-    for name in names:
-        # 이름 출력 텍스트
-        text = "{}: {:.2f}%".format(name, proba * 100)
-        # Mouth_movement 출력 텍스트
-        t2 = "{:.4f}%".format(man[name].Mouth_movement)
-        # 얼굴 테두리 사각형
-        cv2.rectangle(frame, man[name].lefttop, man[name].rightbottom, (0, 0, 255), 2)
-        # 입술 주위 점
-        for (x, y) in man[name].inmarks:
-            cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
-        for (x, y) in man[name].outmarks:
-            cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
-        # 말풍선
-        for (x, y) in man[name].midmarks:
+    if detections.shape[2] > 0:
+        for name in printnames:
+            # 이름 출력 텍스트
+            text = "{}: {:.2f}%".format(name, proba * 100)
+            # Mouth_movement 출력 텍스트
+            t2 = "{:.4f}".format(man[name].Mouth_movement)
+            # 얼굴 테두리 사각형
+            cv2.rectangle(frame, (man[name].sx, man[name].sy), (man[name].ex, man[name].ey), (0, 0, 255), 2)
+            # 입술 주위 점
+            for (x, y) in man[name].Inmarks:
+                cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+            for (x, y) in man[name].Outmarks:
+                cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+            # 말풍선
+            x, y = man[name].Midmark[0,0], man[name].Midmark[0,1] 
             cv2.rectangle(frame, (x - 160, y - 150), (x + 160, y - 70), (255, 255, 255), -1)
-            cv2.rectangle(frame, (x - 160, y - 150), (x + 160, y - 70), (color_a[name], 255, color_b[name]), 3)
+            cv2.rectangle(frame, (x - 160, y - 150), (x + 160, y - 70), (man[name].color_a, 255, man[name].color_b), 3)
             cv2.putText(frame, text, (x-160, y-130), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
             cv2.putText(frame, t2, (x-130, y-100), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
-        # 터미널 출력
-        print(Last_time,'/', name, "'s TOTAL:", man[name].TOTAL_SUB())
+            # 터미널 출력
+            print(Last_time,'/', name, "'s TOTAL:", man[name].Mouth_movement)
+        printnames = []
     ###############################################################
     # update the FPS counter
     fps.update()
