a25d7da03256c2898ec6e1efbbb871789bef5e75
GH-Song <anfflrnrgh@gmail.com>
2019-11-10 12:57:12 +0900

    사진에서 데이터 추출 및 얼굴 인식 학습을 자동으로 수행

---
+++ Face_rec_mouth_det/train_data.py
@@ -0,0 +1,39 @@
+""" 얼굴인식 학습 과정을 자동으로 처리합니다 """
+
+# import the necessary packages
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
+import pickle
+import extract_embeddings
+
+# 이미지 전처리
+extract_embeddings.extract()
+
+path_embeddings = "output/embeddings.pickle" # path to serialized db of facial embeddings
+path_recognizer = "output/recognizer.pickle" # path to output model trained to recognize faces
+path_le = "output/le.pickle" # path to output label encoder
+
+# load the face embeddings
+print("[INFO] loading face embeddings...")
+data = pickle.loads(open(path_embeddings, "rb").read())
+
+# encode the labels
+print("[INFO] encoding labels...")
+le = LabelEncoder()
+labels = le.fit_transform(data["names"])
+
+# train the model used to accept the 128-d embeddings of the face and
+# then produce the actual face recognition
+print("[INFO] training model...")
+recognizer = SVC(C=1.0, kernel="linear", probability=True)
+recognizer.fit(data["embeddings"], labels)
+
+# write the actual face recognition model to disk
+f = open(path_recognizer, "wb")
+f.write(pickle.dumps(recognizer))
+f.close()
+
+# write the label encoder to disk
+f = open(path_le, "wb")
+f.write(pickle.dumps(le))
+f.close()
