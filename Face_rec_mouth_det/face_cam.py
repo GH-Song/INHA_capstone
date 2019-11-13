52770c969c9e43e6fb5cecbc2d66f8f3e1901fed
GH-Song <anfflrnrgh@gmail.com>
2019-11-13 21:55:38 +0900

    신규기능: 사용자 추가 및 제거 가능 인터페이스 편의성 추가 frame 출력 더 잘보이게 수정

--- Face_rec_mouth_det/face_cam.py
+++ Face_rec_mouth_det/face_cam.py
@@ -4,70 +4,118 @@ from imutils.video import FileVideoStream, VideoStream, FPS
 import imutils
 import os
 import time
+import shutil
 
 def face_from_cam():
     face_cascade = cv2.CascadeClassifier('libdata/haarcascade_frontalface_alt.xml')
     path_imwrite = ""
 
     while True:
-        ID = int(input(
-        "이름을 선택하세요(번호 입력), 사진 등록 종료는 -1\n"+
-        "[1] Song_GH\n"+
-        "[2] Kim_JW\n"+
-        "[3] Choi_EH\n"+
-        ">>"))
-        if ID == -1:
+        path_dataset = os.path.join(os.getcwd(),"dataset")
+        names = os.listdir(path_dataset)
+        names.remove("Unknown")
+        paths_of_names = []
+        num_users = len(names)
+
+        # 등록된 사용자 목록 출력
+        print("등록된 사용자 목록-----------------\n")
+        for i, name in enumerate(names):
+            paths_of_names.append(os.path.join(path_dataset, name))
+            print("[{}] {}\n".format(i+1, name))
+        print("[{}] {}\n".format(num_users+1, "신규 사용자 추가"))
+        print("-----------------------------------\n")
+        userkey = input(
+            "사진을 등록할 사용자 번호를 입력하세요 "+
+            "(사진 제거는 해당 번호의 음수 입력, 종료는 q)\n"+ ">> "
+        )
+        if userkey == "q":
             break
-        if ID == 1:
-            path_imwrite = "dataset/Song_GH"
-        elif ID == 2:
-            path_imwrite = "dataset/Kim_JW"
-        elif ID == 3:
-            path_imwrite = "dataset/Choi_EH"
+
+        try:
+            ID = int(userkey)
+
+            # 사용자 삭제
+            if ID < 0:
+                d = abs(ID)-1
+                print("\n[warning]", "삭제 대상:", names[d])
+                key = input("복구할 수 없습니다. 정말로 삭제하시겠습니까?[y/n] >> ")
+                if key == "y":
+                    shutil.rmtree(paths_of_names[d])
+                    print("\n[INFO]", names[d], " 사용자를 삭제했습니다\n")
+                continue
+            else:
+                ID = ID-1
+
+            # 기존 사용자에 사진 추가
+            if ID < num_users:
+                path_imwrite = paths_of_names[ID]
+
+            # 신규 등록
+            elif ID == num_users:
+                new = input("사용자 이름을 입력하세요>>")
+                new_path = os.path.join(path_dataset, new)
+                os.mkdir(new_path)
+                path_imwrite = new_path
+
+        except:
+            print("\n[Error] 정상적인 입력이 아닙니다.\n")
+            continue
+
+        if path_imwrite == "":
+            print("[warn] 사진을 저장할 대상이 없습니다.")
+
         else:
-            path_imwrite = "dataset/Unknown"
+            print("[INFO] starting video stream...")
+            vs = VideoStream(src=0).start()
+            time.sleep(1.0)
 
-        print("[INFO] starting video stream...")
-        vs = VideoStream(src=0).start()
-        time.sleep(1.0)
+            while True:
 
-        while True:
+                frame = imutils.resize(vs.read(), width=1000)
 
-            frame = imutils.resize(vs.read(), width=1000)
-            cv2.putText(frame, "to save picture, press 's'", (100, 130), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 0, 0), 2)
-            cv2.imshow("Frame", frame)
-            key = cv2.waitKey(1) & 0xFF
+                # 안내문 출력
+                cv2.putText(frame, "to save pictures, press 's'", (100, 130),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2.0, (255, 255, 255), 5)
+                cv2.putText(frame, "to save pictures, press 's'", (100, 130),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 0, 0), 3)
+                cv2.putText(frame, "to quit camera, press 'q'", (100, 180),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2.0, (255, 255, 255), 5)
+                cv2.putText(frame, "to quit camera, press 'q'", (100, 180),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 0, 0), 3)
 
-            # if the `q` key was pressed, break from the loop
-            if key == ord("q"):
-                vs.stop()
-                cv2.destroyAllWindows()
-                break
+                cv2.imshow("Frame", frame)
+                key = cv2.waitKey(1) & 0xFF
 
-            elif key == ord("s"):
-                n = 0
-                # 얼굴을 움직이면서 총 사진이 30장 될 때까지 반복?
-                while n < 20:
-                    frame = imutils.resize(vs.read(), width=1000)
-                    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                    faces = face_cascade.detectMultiScale(gray, 1.3,5)
+                # if the `q` key was pressed, break from the loop
+                if key == ord("q"):
+                    vs.stop()
+                    cv2.destroyAllWindows()
+                    break
 
-                    for (x,y,w,h) in faces:
-                        cropped = frame[y - int(h/4):y + h + int(h/4), x - int(w/4):x + w + int(w/4)]
-                        cv2.imwrite(os.path.join(path_imwrite, str(n)+".png"), cropped)
-                        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0),2)
-                        n += 1
+                elif key == ord("s"):
+                    n = 0
+                    # 얼굴을 움직이면서 총 사진이 20장 될 때까지 반복
+                    while n < 20:
+                        frame = imutils.resize(vs.read(), width=1000)
+                        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                        faces = face_cascade.detectMultiScale(gray, 1.3,5)
 
-                    text = "saved: "+str(n)+"/20"
-                    cv2.putText(frame, text, (400, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 2)
-                    cv2.imshow("Frame", frame)
-                    key = cv2.waitKey(1) & 0xFF
+                        for (x,y,w,h) in faces:
+                            cropped = frame[y - int(h/4):y + h + int(h/4), x - int(w/4):x + w + int(w/4)]
+                            cv2.imwrite(os.path.join(path_imwrite, str(n)+".png"), cropped)
+                            cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0),2)
+                            n += 1
 
-                    if key == ord("q"):
-                        vs.stop()
-                        cv2.destroyAllWindows()
-                        break
-                        
+                        text = "saved: "+str(n)+"/20"
+                        cv2.putText(frame, text, (400, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 2)
+                        cv2.imshow("Frame", frame)
+                        key = cv2.waitKey(1) & 0xFF
+
+                        if key == ord("q"):
+                            vs.stop()
+                            cv2.destroyAllWindows()
+                            break
+
 if __name__ == "__main__":
     # execute only if run as a script
     face_from_cam()
